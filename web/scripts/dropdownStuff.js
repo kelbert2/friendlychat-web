e72d8121b722b2acae7a724dca4be018e87bf0d5
Admin <admin@Admins-MacBook-Air.local>
2018-09-16 15:00:10 -0400

    Fixed select box and some coloring

--- web/scripts/dropdownStuff.js
+++ web/scripts/dropdownStuff.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+
+    let select = $('#selectTargetLanguage')
     $.getJSON("scripts/countryjson.json", function (data) {
-        $.each(data, function (index, value) {
-            $('#langOption').append($('<option>').text(value['Country']).attr('value', value['Code']));
+        $.each(data, function (key, value) {
+            // Set the value, creating a new option if necessary
+            /*if (select.find("option[value='" + value['Code'] + "']").length) {
+                //select.val(value['Code']).trigger('change');
+                //    select.append($('<option selected="selected">').attr('value', value['Code']).text(value['Country']));
+            */
+            var newOption;
+            if(value['Code'] == 'en') { // English is the default language
+                // Create a DOM Option and pre-select by default
+                newOption = new Option(value['Country'], value['Code'], true, true);
+            } else {
+                newOption = new Option(value['Country'], value['Code'], false, false);
+            }
+            select.append(newOption)
         });
     });
-    $('.languageOptions').select2();
+    $('.select2-class').select2({
+        dropdownParent: $('.language-selector'), placeholder: 'Select a language', dropdownAutoWidth : true, width: 'auto'
+    });
+    // does flash the placeholder text before finds english, but should have text just in case can't find it
 });
